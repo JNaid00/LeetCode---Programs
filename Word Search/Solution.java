c1fe67e05762f1f7b35d810ba5b382b4592d82f3
Jesse Naidoo <jessenaidoo24@gmail.com>
2022-11-25 11:36:20 +0200

    Added checked field

--- Word Search/Solution.java
+++ Word Search/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
 	public static void main(String[] args) {
 		char board[][] = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
@@ -7,6 +9,7 @@ class Solution {
 		System.out.println(exist(board, "ABCCEDS") ? "ABCCEDS was found" : "ABCCEDS was NOT found");
 		System.out.println(exist(board, "ABCCEDA") ? "ABCCEDA was found" : "ABCCEDA was NOT found");
 		System.out.println();
+		System.out.println("Board 2");
 		char board2[][] =  {{'A','B','C','E'},{'S','F','C','S'}, {'A','D','E','E'}};
 		System.out.println(exist(board2, "SEE") ? "SEE was found" : "SEE was NOT found");
 		System.out.println(exist(board2, "SEEC") ? "SEEC was found" : "SEEC was NOT found");
@@ -14,48 +17,58 @@ class Solution {
 		System.out.println(exist(board2, "DFCCBASFCSEEA") ? "DFCCBASFCSEEA was found" : "DFCCBASFCSEEA was NOT found");
 
 		System.out.println(exist(board2, "ABCB") ? "ABCB was found" : "ABCB was NOT found");
-		
+		System.out.println(exist(board2, "CEECCESC") ? "CEECCESC was found" : "CEECCESC was NOT found");
+		System.out.println(exist(board2, "A") ? "A was found" : "A was NOT found");
+		System.out.println(exist(board2, "Z") ? "Z was found" : "Z was NOT found");
 	}
     public static boolean exist(char[][] board, String word) {
         int pos[] = {0};
+		boolean checked[][] = new boolean[board.length][board[0].length];
+		
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (word.charAt(pos[0]) == board[i][j]){
 					pos[0]++;
-					boolean ans = solveRecur(board,word, pos, i, j);
+					checked[i][j] = true;
+					boolean ans = solveRecur(board,word, pos, i, j, checked);
 
 					if(ans == true){
 						return true;
 					}
 					pos[0] = 0;
+					checked[i][j] = false;
 				}
 			}
 		}
 		return false;
     }
-	private static boolean solveRecur(char[][] board, String word, int[] pos, int i, int j) {
+	private static boolean solveRecur(char[][] board, String word, int[] pos, int i, int j, boolean[][] checked) {
 		if (word.length() == pos[0]){
 			return true;
 		}
 		boolean ans = false;
 		int original = pos[0];
+		boolean copy[][] = Arrays.stream(checked).map(boolean[]::clone).toArray(boolean[][]::new);
 		//Try upwards first
-		if (dimensions(i-1, j, board.length, board[0].length) == true){
+		if (dimensions(i-1, j, board.length, board[0].length) == true && checked[i-1][j] == false){
 			if (board[i-1][j] == word.charAt(pos[0])){
 				pos[0]++;
-				ans = solveRecur(board,word, pos, i - 1, j);
+				checked[i-1][j] = true;
+				ans = solveRecur(board,word, pos, i - 1, j, checked);
 			}
 		}
 		if (ans == true){
 			return true;
 		}
 		ans = false;
+		checked = copy;
 		pos[0] = original;
 		//Try to the right
-		if (dimensions(i, j+1, board.length, board[0].length) == true){
+		if (dimensions(i, j+1, board.length, board[0].length) == true && checked[i][j+1] == false){
 			if (board[i][j+1] == word.charAt(pos[0])){
 				pos[0]++;
-				ans = solveRecur(board,word, pos, i, j+1);
+				checked[i][j+1] = true;
+				ans = solveRecur(board,word, pos, i, j+1, checked);
 			}
 		}
 
@@ -63,13 +76,15 @@ class Solution {
 			return true;
 		}
 		ans = false;
+		checked = copy;
 		pos[0] = original;
 
 		//Try downwards
-		if (dimensions(i+1, j, board.length, board[0].length) == true){
+		if (dimensions(i+1, j, board.length, board[0].length) == true && checked[i+1][j] == false){
 			if (board[i+1][j] == word.charAt(pos[0])){
 				pos[0]++;
-				ans = solveRecur(board,word, pos, i+1, j);
+				checked[i+1][j] = true;
+				ans = solveRecur(board,word, pos, i+1, j, checked);
 			}
 		}
 
@@ -77,18 +92,21 @@ class Solution {
 			return true;
 		}
 		ans = false;
+		checked = copy;
 		pos[0] = original;
 
 		//Try to the left
-		if (dimensions(i, j-1, board.length, board[0].length) == true){
+		if (dimensions(i, j-1, board.length, board[0].length) == true && checked[i][j-1] == false){
 			if (board[i][j-1] == word.charAt(pos[0])){
 				pos[0]++;
-				ans = solveRecur(board,word, pos, i, j-1);
+				checked[i][j-1] = true;
+				ans = solveRecur(board,word, pos, i, j-1, checked);
 			}
 		}
 		if (ans == true){
 			return true;
 		}
+		checked = copy;
 		pos[0] = original;
 
 		return false;
