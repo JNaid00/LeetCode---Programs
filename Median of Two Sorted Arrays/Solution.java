8085500538094186b2c3e361eb0c521df9c84c12
Jesse Naidoo <jessenaidoo24@gmail.com>
2022-11-25 11:59:31 +0200

    Fixed Two Product problem

---
+++ Median of Two Sorted Arrays/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+	public void main(String[] args) {
+		int nums1[] = {1,3}, nums2 []= {2};
+		System.out.println(findMedianSortedArrays(nums1, nums2));
+	}	
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+		
+		return 0;
+    }
+}
