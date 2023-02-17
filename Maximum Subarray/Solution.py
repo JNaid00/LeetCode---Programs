20796a476f2e4df96e6401c0f7e8a7e2c4136553
JNAIDOO <jessenaidoo24@gmail.com>
2023-02-17 22:52:52 +0200

    Kadane's Algorithm

--- Maximum Subarray/Solution.py
+++ Maximum Subarray/Solution.py
@@ -5,23 +5,19 @@ class Solution(object):
         :rtype: int
         """
         if len(nums) == 1: return nums[0]
-        ans = nums[0]
+        if len(nums) == 0: return 0
         
-        for i in range(len(nums)):
-            test = nums[i]
-            if test > ans: ans = test
-            for j in range(i + 1, len(nums)):
-                test += nums[j]
-                if test > ans: ans = test
+        sum_max = nums[0]
+        sum_including_current = nums[0]
 
-        return ans
+        for i in range(1,len(nums)):
+            sum_including_current = max(sum_including_current + nums[i], nums[i])
+            sum_max = max(sum_max, sum_including_current)
+
+        return sum_max
 
 sol = Solution()
 print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 print(sol.maxSubArray([5,4,-1,7,8]))
 print(sol.maxSubArray([5,-23,4,-1,7,8,2]))
 print(sol.maxSubArray([1]))
-
-
-
-
