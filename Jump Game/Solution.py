e9c0fe88fa51556e633daa7c1f8c40e49e46f4ad
JNAIDOO <jessenaidoo24@gmail.com>
2023-02-19 16:25:38 +0200

    Greedy solution

--- Jump Game/Solution.py
+++ Jump Game/Solution.py
@@ -10,7 +10,6 @@ class Solution(object):
             numsI = nums[i]
             if i + nums[i] >= goal:
                 goal = i
-        
         if goal == 0: return True
         return False
 
@@ -21,5 +20,4 @@ print(sol.canJump([3,2,1,2,0,4]))
 print(sol.canJump([2,3,1,1,4,0,0,0,0]))
 print(sol.canJump([2,3,1,1,4,0,0,1]))
 print(sol.canJump([5,2,1,2,0,4]))
-
 
