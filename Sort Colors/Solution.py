310ef102a7e0e6ba7de85f86658c28e5fb49d7f3
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-07-07 21:41:35 +0200

    2 pointer solution

--- Sort Colors/Solution.py
+++ Sort Colors/Solution.py
@@ -5,13 +5,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                temp = nums.pop(i)
-                nums.insert(0, temp)
-            elif nums[i] == 2:
-                temp = nums.pop(i)
-                nums.insert(len(nums), temp)
+        start, end = 0, len(nums)-1
+        curr = 0
+
+        while curr <= end:
+            if nums[curr] == 2:
+                temp = nums[curr]
+                nums[curr] = nums[end]
+                nums[end] = temp
+                end -= 1
+            elif nums[curr] == 0:
+                temp = nums[curr]
+                nums[curr] = nums[start]
+                nums[start] = temp
+                start += 1
+                curr += 1
+            else:#if 1
+                curr += 1
+                
         
 
 sol = Solution()
@@ -21,7 +32,7 @@ print(arr)
 arr = [2,0,1]
 sol.sortColors(arr)
 print(arr)
-arr = [2,0,2,1,1,0,0,0,0,0,0,]
+arr = [2,0,2,1,1,0,0,0,0,0,0]
 sol.sortColors(arr)
 print(arr)
 
